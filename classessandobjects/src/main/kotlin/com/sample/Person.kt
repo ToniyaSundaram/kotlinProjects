a5798eb4584e4db04cb5dc2e8b001ab926371061
Toniya <toniya.sundaram@gmail.com>
2017-12-01 22:06:37 +0530

    Adding Companion Object which is static in java

--- classessandobjects/src/main/kotlin/com/sample/Person.kt
+++ classessandobjects/src/main/kotlin/com/sample/Person.kt
@@ -4,15 +4,40 @@ interface Signatory {
     fun sign()
 }
 
-class Person (val name:String, var age: Int): Signatory {
-    override fun sign() = println("$name aged $age can override your signature")
+open class Person (val name:String, var age: Int): Signatory {
+
+    var patnername: String = ""
+    override fun sign() = println("$name aged $age can override your signature $isMarried")
+    var isMarried= false
+    constructor(name:String, age:Int,isMarried:Boolean): this(name,age) {
+        this.isMarried=isMarried
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            val p = Person("Toniya",18,true)
+            // p.sign()
+            p.patnername = "Deepu"
+            println("My friend's name is ${p.patnername}")
+
+            p.patnername = "Pappu"
+            println("My friend's name is ${p.patnername}")
+
+
+            p.age = 20
+            p.sign()
+
+        }
+    }
 }
 
 
 
 fun main(args: Array<String>) {
-    val p = Person("Toniya",18)
-    p.sign()
+    val p = Person("Toniya",18,true)
+   // p.sign()
     p.age = 20
     p.sign()
 
