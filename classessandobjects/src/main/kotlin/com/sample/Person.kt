206ee52ef5f22aeb12929ea1ee75429e06106b9f
Toniya <toniya.sundaram@gmail.com>
2017-12-01 22:04:00 +0530

    Revert "Adding Constructor to the existing class"
    This reverts commit 347b964

--- classessandobjects/src/main/kotlin/com/sample/Person.kt
+++ classessandobjects/src/main/kotlin/com/sample/Person.kt
@@ -4,40 +4,15 @@ interface Signatory {
     fun sign()
 }
 
-open class Person (val name:String, var age: Int): Signatory {
-
-    var patnername: String = ""
-    override fun sign() = println("$name aged $age can override your signature $isMarried")
-    var isMarried= false
-    constructor(name:String, age:Int,isMarried:Boolean): this(name,age) {
-        this.isMarried=isMarried
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-
-            val p = Person("Toniya",18,true)
-            // p.sign()
-            p.patnername = "Deepu"
-            println("My friend's name is ${p.patnername}")
-
-            p.patnername = "Pappu"
-            println("My friend's name is ${p.patnername}")
-
-
-            p.age = 20
-            p.sign()
-
-        }
-    }
+class Person (val name:String, var age: Int): Signatory {
+    override fun sign() = println("$name aged $age can override your signature")
 }
 
 
 
 fun main(args: Array<String>) {
-    val p = Person("Toniya",18,true)
-   // p.sign()
+    val p = Person("Toniya",18)
+    p.sign()
     p.age = 20
     p.sign()
 
