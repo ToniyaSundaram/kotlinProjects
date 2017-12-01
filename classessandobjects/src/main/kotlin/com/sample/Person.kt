d0669c6ba84b223a9e7ff9dc7e0f57fba41b9b7a
Toniya <toniya.sundaram@gmail.com>
2017-12-01 19:54:39 +0530

    Adding Constructor to the existing class

--- classessandobjects/src/main/kotlin/com/sample/Person.kt
+++ classessandobjects/src/main/kotlin/com/sample/Person.kt
@@ -4,11 +4,16 @@ interface Signatory {
     fun sign()
 }
 
-class Person : Signatory {
-    override fun sign() = println("I can override your signature")
+class Person (val name:String, var age: Int): Signatory {
+    override fun sign() = println("$name aged $age can override your signature")
 }
 
+
+
 fun main(args: Array<String>) {
-    val p = Person()
+    val p = Person("Toniya",18)
     p.sign()
+    p.age = 20
+    p.sign()
+
 }
