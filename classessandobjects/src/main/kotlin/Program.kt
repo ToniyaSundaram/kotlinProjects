704fa1cb3d7864c0a81794ac5aa75f7450aafb82
Toniya <toniya.sundaram@gmail.com>
2017-12-02 15:47:14 +0530

    Adding collections module

--- classessandobjects/src/main/kotlin/Program.kt
+++ classessandobjects/src/main/kotlin/Program.kt
@@ -2,14 +2,44 @@ import com.rsk.Providers
 
 fun main(args: Array<String>) {
 
+    val provider = Providers()
+    val details = provider.getallProviders()
+    details.forEach (::println)
+
+   getallProviders {
+        key,value -> println("\t---The key is : $key and the value is : $value")
+    }
+}
+
+fun getallProviders (fn: (String, String)->Unit) {
     val providers = Providers()
     val allProviders = providers.getProviders()
-     val it = allProviders.iterator()
+    val it = allProviders.iterator()
+
+    while (it.hasNext()) {
+        val provider = it.next()
+        println(provider.name)
+        provider.forEach{key,value -> fn(key.toString(),value.toString())}
+    }
+}
+
+
+
+fun listProvidersInstance  () {
+    val providers = Providers()
+    val allProviders = providers.getProviders()
+    val it = allProviders.iterator()
 
     while (it.hasNext()) {
         val provider = it.next()
         println(provider.name)
         provider.forEach{key,value -> println("\tThe key is : $key and the value is : $value")}
     }
+
 }
+
+
+
+
+
 
--- classessandobjects/src/main/kotlin/com/rsk/Providers.kt
+++ classessandobjects/src/main/kotlin/com/rsk/Providers.kt
@@ -3,11 +3,35 @@ package com.rsk
 import java.security.Provider
 import java.security.Security
 
+data class ProviderDetails(val providername:String, val name:String)
+
 class Providers {
 
     fun getProviders ():List<Provider> {
         val providers = Security.getProviders()
         val listOfProviders: List<Provider> = providers.asList()
         return listOfProviders
+    }
+
+    fun getallProviders ():List<ProviderDetails> {
+        val providers = Security.getProviders()
+        val listOfProviders= mutableListOf<ProviderDetails>()
+
+
+        providers.forEach {
+            provider ->
+            val providerDetail = provider.entries.map { ProviderDetails(provider.name,it.key.toString()) }
+            listOfProviders += providerDetail
+        }
+
+        return listOfProviders
+    }
+
+    companion object {
+        fun getProviders ():List<Provider> {
+            val providers = Security.getProviders()
+            val listOfProviders: List<Provider> = providers.asList()
+            return listOfProviders
+        }
     }
 }
